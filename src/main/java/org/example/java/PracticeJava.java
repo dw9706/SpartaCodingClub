1f5fd5e27ae682433938f5e04ba68b66cee7a964
kodongwon <dw9706@gmail.com>
2025-04-02 22:44:42 +0900

    반복문 연습하기 Part 1

---
+++ src/main/java/org/example/java/PracticeJava.java
@@ -0,0 +1,15 @@
+package org.example.java;
+
+public class PracticeJava {
+    public static void main(String[] args) {
+        print1To100();
+
+    }
+
+    public static void print1To100() {
+        // 반복문 연습하기 Part 1
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
