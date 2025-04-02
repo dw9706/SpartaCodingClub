d1ef1bc04b22b8d685fd017baf751ad326d6e6f8
kodongwon <dw9706@gmail.com>
2025-04-02 22:49:57 +0900

    반복문 연습하기 Part 2

--- src/main/java/org/example/java/PracticeJava.java
+++ src/main/java/org/example/java/PracticeJava.java
@@ -3,13 +3,20 @@ package org.example.java;
 public class PracticeJava {
     public static void main(String[] args) {
         print1To100();
-
+        printEvenNumbersFrom1To100();
     }
 
     public static void print1To100() {
         // 반복문 연습하기 Part 1
         for (int i = 1; i <= 100; i++) {
             System.out.println(i);
+        }
+    }
+
+    public static void printEvenNumbersFrom1To100() {
+        // 반복문 연습하기 Part 2
+        for (int i = 1; i <= 100 ; i++) {
+            if(i%2==0) System.out.println(i);
         }
     }
 }
