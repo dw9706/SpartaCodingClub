1724b840a519f19b401fa2a3ad8304fa4c843576
kodongwon <dw9706@gmail.com>
2025-04-02 22:52:46 +0900

    반복문 연습하기 Part 3

--- src/main/java/org/example/java/PracticeJava.java
+++ src/main/java/org/example/java/PracticeJava.java
@@ -4,6 +4,7 @@ public class PracticeJava {
     public static void main(String[] args) {
         print1To100();
         printEvenNumbersFrom1To100();
+        gugudan();
     }
 
     public static void print1To100() {
@@ -17,6 +18,15 @@ public class PracticeJava {
         // 반복문 연습하기 Part 2
         for (int i = 1; i <= 100 ; i++) {
             if(i%2==0) System.out.println(i);
+        }
+    }
+
+    public static void gugudan() {
+        // 반복문 연습하기 Part 3
+        for (int i = 2; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
         }
     }
 }
